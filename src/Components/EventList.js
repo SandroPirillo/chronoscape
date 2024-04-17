d858840cb95336db1d652097ef7262abea313998
SandroPirillo <sandropirillo@hotmail.com>
2024-04-17 21:53:10 +1000

    added colour to the barcharts in eventlist

--- src/Components/EventList.js
+++ src/Components/EventList.js
@@ -1,35 +1,73 @@
 import React from "react";
-import EventGroupingAndComparisionHelper from '../Utils/EventGroupingAndComparisionHelper.js';
-import { Chart, BarElement, CategoryScale, LinearScale, Tooltip, Legend} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import './EventList.css';
-
+import EventGroupingAndComparisionHelper from "../Utils/EventGroupingAndComparisionHelper.js";
+import {
+  Chart,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import "./EventList.css";
 
 Chart.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
-function EventList({ group, onBack }) {
+function colourIdConverter(id) {
+  /**
+
+   */
+  switch (id) {
+    case "1":
+      return "rgba(121, 134, 203, 0.6)";
+    case "2":
+      return "rgba(74, 113, 108, 0.6)";
+    case "3":
+      return "rgba(142, 36, 170, 0.6)";
+    case "4":
+      return "rgba(51, 182, 121, 0.6)";
+    case "5":
+      return "rgba(230, 124, 115, 0.6)";
+    case "6":
+      return "rgba(246, 192, 38, 0.6)";
+    case "7":
+      return "rgba(245, 81, 29, 0.6)";
+    case "8":
+      return "rgba(3, 155, 229, 0.6)";
+    case "9":
+      return "rgba(97, 97, 97, 0.6)";
+    case "10":
+      return "rgba(11, 128, 67, 0.6)";
+    case "11":
+      return "rgba(63, 81, 181, 0.6)";
+    default:
+      return "rgba(3, 155, 229, 0.6)";
+  }
+}
+const EventList = ({ group, onBack }) => {
+  console.log(group.events.map((event) => event.colour));
   const data = {
-    labels: group.events.map(event => event.name),
+    labels: group.events.map((event) => event.name),
     datasets: [
       {
-        label: 'Total Time in Hours',
-        data: group.events.map(event => event.totalTime/ 3600000), // convert milliseconds to hours
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
+        label: "Total Time in Hours",
+        data: group.events.map((event) => event.totalTime / 3600000), // convert milliseconds to hours
+        backgroundColor: group.events.map((event) => colourIdConverter(event.colour)),
+        borderColor: group.events.map((event) => colourIdConverter(event.colour)),
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(75,192,192,0.6)',
-        hoverBorderColor: 'rgba(75,192,192,1)',
-      }
-    ]
+        hoverBackgroundColor: group.events.map((event) => colourIdConverter(event.colour)),
+        hoverBorderColor: group.events.map((event) => colourIdConverter(event.colour)),
+      },
+    ],
   };
   return (
     <div>
       <button onClick={onBack}>Back to groups</button>
-    <div className="bar-chart">
-       <Bar data={data} />
-    </div>
+      <div className="bar-chart">
+        <Bar data={data} />
+      </div>
     </div>
   );
-}
+};
 
 export default EventList;
