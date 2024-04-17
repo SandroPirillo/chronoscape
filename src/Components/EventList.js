51596ea9d45324b377ba98f69e255c25c17470f0
SandroPirillo <sandropirillo@hotmail.com>
2024-04-17 15:40:21 +1000

    can display bar chart for EventList

--- src/Components/EventList.js
+++ src/Components/EventList.js
@@ -1,19 +1,33 @@
 import React from "react";
+import EventGroupingAndComparisionHelper from '../Utils/EventGroupingAndComparisionHelper.js';
+import { Chart, BarElement, CategoryScale, LinearScale, Tooltip, Legend} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import './EventList.css';
+
+
+Chart.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 function EventList({ group, onBack }) {
+  const data = {
+    labels: group.events.map(event => event.name),
+    datasets: [
+      {
+        label: 'Total Time in Hours',
+        data: group.events.map(event => event.totalTime/ 3600000), // convert milliseconds to hours
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(75,192,192,0.6)',
+        hoverBorderColor: 'rgba(75,192,192,1)',
+      }
+    ]
+  };
   return (
     <div>
       <button onClick={onBack}>Back to groups</button>
-      {group.events.map((event) => () => {
-        return (
-          <div key={event.id}>
-            <h2>{event.summary}</h2>
-            <p>{event.description}</p>
-            <p>{event.start.dateTime}</p>
-            <p>{event.end.dateTime}</p>
-          </div>
-        );
-      })}
+    <div className="bar-chart">
+       <Bar data={data} />
+    </div>
     </div>
   );
 }
